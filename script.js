bcd99258e7dbd5dd64372dfd071f500ab17896ff
NGAM TARYLL <taryllngam@gmail.com>
2023-04-19 20:54:03 +0100

    addeed functionalty to work with js

---
+++ script.js
@@ -0,0 +1,39 @@
+const task = document.getElementById('task');
+const list = document.getElementById('list');
+
+function addTask() {
+    if (task.value.length <= 0) {
+        alert('please enter a task')
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = task.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
+    }
+    task.value = "";
+    saveData();
+}
+
+list.addEventListener('click', function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked")
+        saveData();
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveData();
+    }
+}, false);
+
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask() {
+    list.innerHTML = localStorage.getItem("data");
+}
+showTask()
