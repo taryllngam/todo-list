251ba9c7dfae7918e6d3b5f71590a87a5b145ae1
NGAM TARYLL <taryllngam@gmail.com>
2023-04-20 10:25:24 +0100

    my linter checks

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-const task = document.getElementById('task');
-const list = document.getElementById('list');
+const task = document.getElementById("task");
+const list = document.getElementById("list");
 
 function addTask() {
-    if (task.value.length <= 0) {
-        alert('please enter a task')
-    }
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = task.value;
-        list.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span)
-    }
-    task.value = "";
-    saveData();
+  if (task.value.length <= 0) {
+    alert("please enter a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = task.value;
+    list.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  task.value = "";
+  saveData();
 }
 
-list.addEventListener('click', function (e) {
+list.addEventListener(
+  "click",
+  function (e) {
     if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked")
-        saveData();
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
     }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove()
-        saveData();
-    }
-}, false);
-
+  },
+  false
+);
 
 function saveData() {
-    localStorage.setItem("data", list.innerHTML);
+  localStorage.setItem("data", list.innerHTML);
 }
 
 function showTask() {
-    list.innerHTML = localStorage.getItem("data");
+  list.innerHTML = localStorage.getItem("data");
 }
-showTask()
+showTask();
