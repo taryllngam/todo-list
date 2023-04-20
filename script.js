edf73385b251baa0d111b19c92da78d7a045f1d5
NGAM TARYLL <taryllngam@gmail.com>
2023-04-20 10:33:37 +0100

    my linters corrections

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-const task = document.getElementById("task");
-const list = document.getElementById("list");
+const task = document.getElementById('task')
+const list = document.getElementById('list')
 
-function addTask() {
+function addTask () {
   if (task.value.length <= 0) {
-    alert("please enter a task");
+    alert('please enter a task')
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = task.value;
-    list.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
+    const li = document.createElement('li')
+    li.innerHTML = task.value
+    list.appendChild(li)
+    const span = document.createElement('span')
+    span.innerHTML = '\u00d7'
+    li.appendChild(span)
   }
-  task.value = "";
-  saveData();
+  task.value = ''
+  saveData()
 }
 
 list.addEventListener(
-  "click",
+  'click',
   function (e) {
-    if (e.target.tagName === "LI") {
-      e.target.classList.toggle("checked");
-      saveData();
-    } else if (e.target.tagName === "SPAN") {
-      e.target.parentElement.remove();
-      saveData();
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked')
+      saveData()
+    } else if (e.target.tagName === 'SPAN') {
+      e.target.parentElement.remove()
+      saveData()
     }
   },
   false
-);
+)
 
-function saveData() {
-  localStorage.setItem("data", list.innerHTML);
+function saveData () {
+  localStorage.setItem('data', list.innerHTML)
 }
 
-function showTask() {
-  list.innerHTML = localStorage.getItem("data");
+function showTask () {
+  list.innerHTML = localStorage.getItem('data')
 }
-showTask();
+showTask()
